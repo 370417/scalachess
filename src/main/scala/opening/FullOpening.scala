f70b4e9f4179357f7b6d6da4ef9eb3fcc785f75c
Thibault Duplessis <t@lichess.org>
2022-06-10 11:29:10 +0200

    add OpeningFamily and OpeningVariation

--- src/main/scala/opening/FullOpening.scala
+++ src/main/scala/opening/FullOpening.scala
@@ -7,6 +7,12 @@ final class FullOpening(
     val fen: String
 ):
 
+  val (family: OpeningFamily, variation: Option[OpeningVariation]) = name.split(":", 2) match {
+    case Array(f, v) => OpeningFamily(f)    -> Some(OpeningVariation(v.takeWhile(',' !=)))
+    case Array(f)    => OpeningFamily(f)    -> None
+    case _           => OpeningFamily(name) -> None
+  }
+
   def ecoName = s"$eco $name"
 
   override def toString = ecoName
@@ -16,3 +22,6 @@ final class FullOpening(
 object FullOpening:
 
   case class AtPly(opening: FullOpening, ply: Int)
+
+case class OpeningFamily(val name: String)
+case class OpeningVariation(val name: String)
